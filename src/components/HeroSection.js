e7d6558a024e66ccf0129afe8ec9041946ba4870
IMANI <kiplagatfaith88@gmail.com>
2025-08-04 20:46:08 +0300

    setup Githubm pages deployment

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,7 +8,7 @@ function HeroSection() {
   return (
     <div className='hero-container' >  
       <video src="/travel-intro.mp4" autoPlay loop muted />    
-      <h1>EXPLORE THE WORLD</h1>
+      <h1>EXPLORE KENYA</h1>
       <p>SO WHAT IS HOLDING YOU BACK?</p>
       <div className='hero-btns'> 
         <Button className='btns' buttonStyle= 'btn--outline'  buttonSize='btn--large'>GET STARTED</Button>
